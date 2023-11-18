33f62355416a5c745ef61b2a7c010587a50f7220
Ubuntu <ubuntu@ip-172-31-28-215.ec2.internal>
2023-11-18 21:09:57 +0000

    information retrieval for context by sentence -> need large GPU mem

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # question_answering
 Question Generation and Answering System for 11-611 CMU 
+
+- install dependencies
+	- for ec2 terminal
+		source activate pytorch
+	- othewise enter your local python environment
+	pip install -r requirements.txt
--- utils.py
+++ utils.py
@@ -4,17 +4,22 @@ import pickle as pkl
 from tqdm import tqdm
 import copy
 
+import nltk
+nltk.download('punkt')
+from nltk import tokenize
+
 import torch
 from torch.utils.data import DataLoader, Dataset
+from datasets import load_dataset
 
-from transformers import T5Config, T5ForConditionalGeneration, T5Tokenizer
+from transformers import T5Config, T5ForConditionalGeneration, T5Tokenizer, T5Model
 
 
 class CustomData(Dataset):
   '''
     Process raw data
   '''
-  def __init__(self, file_dir):
+  def __init__(self, file_dir, model, tokenzier, k=1):
       self.file = file_dir
       self.article_name = []
       self.questions = []
@@ -22,18 +27,25 @@ class CustomData(Dataset):
       self.q_diffi = []
       self.a_diffi = []
       self.article_path = []
+      self.context_nn = {}
       self.context = {} # only fill when load the dataset
+      self.context_embed = {}
+
+      device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+      self.model = model.to(device)
+      self.tok = tokenzier
 
       # get question answer pairs
       for div in ['S08', 'S09', 'S10']:
         skip = True
         qa_path = os.path.join(self.file, div, "question_answer_pairs.txt")
+        num_lines = sum(1 for line in open(qa_path,'rb'))
         with open(qa_path, 'rb') as f:
-          for line in f:
+          for line in tqdm(f, total=num_lines):
             if skip:
-              skip = False
+              skip = False # skip the first line
               continue
-            try:
+            try: # only continue if the decoding is valid for utf-8
               row = line.decode().split('\t')
             except:
               continue
@@ -44,20 +56,53 @@ class CustomData(Dataset):
             context_file = self.file + "/" + div + "/"+ row[5][:-1] + ".txt" # path to the context file
             if not (os.path.exists(context_file) and os.path.isfile(context_file)): # otherwise context doesn't exist: invalid
               continue
-            # check if context could be extracted
-            try:
-              with open(context_file, 'rb') as f:
-                curr_context = f.read().decode().replace('\n',' ')
-            except:
-              continue
 
+            # only process document embedding when needed (article first found)
+            if row[0] not in self.context_embed.keys():
+              # check if context could be extracted
+              try:
+                with open(context_file, 'rb') as f:
+                  curr_context = f.read().decode() # could be decoded, otherwise skip
+              except:
+                continue
+
+              curr_context = curr_context.split('Related Wikipedia Articles')[0] # ignore everything after Related articles
+              curr_context = curr_context.replace('\n',' ')
+              self.context[row[0]] = tokenize.sent_tokenize(curr_context)
+              # encode context and add to corresponding files
+              c_embed = []
+              for context in self.context[row[0]]:
+                enc = self.tok(context, max_length=512, padding='max_length', return_tensors="pt")
+                output = self.model.encoder(
+                  input_ids=enc["input_ids"].to(device),
+                  attention_mask=enc["attention_mask"].to(device),
+                  return_dict=True
+                )
+                c_embed.append(output.last_hidden_state)
+              self.context_embed[row[0]] = torch.cat(c_embed)
+
+            # get top-1 similar context
+            qa_enc = self.tok(context, max_length=512, padding='max_length', return_tensors="pt")
+            qa_output = self.model.encoder(
+                  input_ids=qa_enc["input_ids"].to(device),
+                  attention_mask=qa_enc["attention_mask"].to(device),
+                  return_dict=True
+            )
+            encoded_qa = qa_output.last_hidden_state # question and answer embedding
+            c_embed = self.context_embed[row[0]] # load the context embeddings
+            # compute knn score: dot product
+            scores = c_embed.matmul(encoded_qa)
+            nn = torch.argmax(scores)
+            # the text of the closest neighbor
+            self.context_nn[row[0]] = self.context[row[0]][nn]
+
+            # other info
             self.article_name.append(row[0])
             self.questions.append(row[1])
             self.answers.append(row[2])
-            self.q_diffi.append(row[3])
+            self.q_diffi.append(row[3]) # difficulty
             self.a_diffi.append(row[4])
             self.article_path.append(div + "/"+ row[5][:-1]) # get rid of '\n
-            self.context[row[0]] = curr_context # article_name: context
 
       print("length of dataset: ", len(self.questions))
 
@@ -65,8 +110,22 @@ class CustomData(Dataset):
       return len(self.questions)
 
   def __getitem__(self, idx):
-      # retrieve context here -> less mem storage overhead
       return self.questions[idx], self.answers[idx], self.context[self.article_name[idx]]
+    
+# dump the raw dataset to json file
+def dump_set(data, output_file):
+  l = []
+  for q, a, c in data:
+    l.add({"question": q, "answer": a, "context": context})
+  with open(output_file, 'w') as fp:
+      json.dump(l, fp)
+
+
+# load the raw dataset from json file as a dataset object
+def load_json_data(json_file):
+  test_dataset = load_dataset('json', data_files={'all':json_file})
+  return test_dataset['all']
+
 
 
 # tokenize the text features
