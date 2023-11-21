0bee376397f1edcdebbdec06d360204093ed8421
Ubuntu <ubuntu@ip-172-31-18-118.ec2.internal>
2023-11-21 20:12:23 +0000

    try to encode dataset with Roberta

--- utils.py
+++ utils.py
@@ -219,3 +219,100 @@ def get_model(checkpoint: str, device: str, tokenizer: T5Tokenizer) -> T5ForCond
     model = model.to(device)
     return model
 
+
+# encode embeddings by Roberta-base
+class CustomAData(Dataset):
+    '''
+    Process raw data
+    '''
+    def __init__(self, file_dir, model, tokenzier, k=1):
+        self.file = file_dir
+        self.article_name = []
+        self.questions = []
+        self.answers = []
+        self.q_diffi = []
+        self.a_diffi = []
+        self.article_path = []
+        self.context_nn = {}
+        self.context = {} # only fill when load the dataset
+        self.context_embed = {}
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = model.to(device)
+        self.tok = tokenzier
+        model.eval()
+        
+        # get question answer pairs
+        for div in ['S08', 'S09', 'S10']:
+            skip = True
+            qa_path = os.path.join(self.file, div, "question_answer_pairs.txt")
+            num_lines = sum(1 for line in open(qa_path,'rb'))
+            with open(qa_path, 'rb') as f:
+
+                for line in tqdm(f, total=num_lines):
+
+                    if skip:
+                        skip = False # skip the first line
+                        continue
+
+                    try: # only continue if the decoding is valid for utf-8
+                        row = line.decode().split('\t')
+                    except:
+                        continue
+
+                    if "NULL" in row:
+                        continue # if any feature does not exist -> skip
+
+                    context_file = self.file + "/" + div + "/"+ row[5][:-1] + ".txt" # path to the context file
+                    if not (os.path.exists(context_file) and os.path.isfile(context_file)): # otherwise context doesn't exist: invalid
+                        continue
+
+                    # only process document embedding when needed (article first found)
+                    if row[0] not in self.context_embed.keys():
+                        # check if context could be extracted
+                        try:
+                            with open(context_file, 'rb') as f:
+                                curr_context = f.read().decode() # could be decoded, otherwise skip
+                        except:
+                            continue
+
+                        curr_context = curr_context.split('Related Wikipedia Articles')[0] # ignore everything after Related articles
+                        curr_context = curr_context.replace('\n',' ')
+                        self.context[row[0]] = tokenize.sent_tokenize(curr_context)
+                        # encode context and add to corresponding files
+                        c_embed = []
+                        for context in self.context[row[0]]:
+                            enc = self.tok(context, max_length=512, padding='max_length', return_tensors="pt")
+                            enc.to(device)
+                            output = self.model(**enc)
+                            c_embed.append(output.last_hidden_state)
+                            self.context_embed[row[0]] = torch.cat(c_embed)
+
+                    # get top-1 similar context
+                    qa_enc = self.tok(context, max_length=512, padding='max_length', return_tensors="pt")
+                    qa_enc.to(device)
+                    qa_output = self.model.encoder(**qa_enc)
+                    encoded_qa = qa_output.last_hidden_state # question and answer embedding
+                    c_embed = self.context_embed[row[0]] # load the context embeddings
+                    # compute knn score: dot product
+                    scores = c_embed.matmul(encoded_qa)
+                    nn = torch.argmax(scores)
+                    # the text of the closest neighbor
+                    self.context_nn[row[0]] = self.context[row[0]][nn]
+
+                    # other info
+                    self.article_name.append(row[0])
+                    self.questions.append(row[1])
+                    self.answers.append(row[2])
+                    self.q_diffi.append(row[3]) # difficulty
+                    self.a_diffi.append(row[4])
+                    self.article_path.append(div + "/"+ row[5][:-1]) # get rid of '\n
+
+        print("length of dataset: ", len(self.questions))
+
+    def __len__(self):
+        return len(self.questions)
+
+    def __getitem__(self, idx):
+        return self.questions[idx], self.answers[idx], self.context[self.article_name[idx]]
+
